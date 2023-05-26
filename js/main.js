ed58c639ed699d55b1f8c20657f07e4956d16132
=ahmedyasser <=ahmedya3ser8@gmail.com>
2023-05-26 06:44:26 +0300

    Creating Settings Box And Save Colors In Localstorage

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+// Save Color In LocalStorage
+let mainColor = localStorage.getItem("color-option");
+if (mainColor !== null) {
+  document.documentElement.style.setProperty("--main-color", mainColor);
+}
+
+// Click On Toggle Settings Icon
+document.querySelector(".toggle-settings i").onclick = function() {
+  this.classList.toggle("fa-spin");
+  document.querySelector(".settings-box").classList.toggle("open");
+}
+
+// Switch Colors
+const colorsLi = document.querySelectorAll(".colors-list li");
+colorsLi.forEach(li => {
+  li.addEventListener("click", (e) => {
+    document.documentElement.style.setProperty("--main-color", e.target.dataset.color);
+    localStorage.setItem("color-option", e.target.dataset.color);
+    e.target.parentElement.querySelectorAll(".active").forEach(element => {
+      element.classList.remove("active");
+    });
+    e.target.classList.add("active");
+  });
+});
+
 // Random Background Images
 let landingPage = document.querySelector(".landing-page");
 let imagesArray = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
