d3217a30b25603f3ffba9b4fd268d3fc81bbf408
=ahmedyasser <=ahmedya3ser8@gmail.com>
2023-05-27 06:03:04 +0300

    Finish RandomBackground and Save It In LocalStorage

--- js/main.js
+++ js/main.js
@@ -1,7 +1,32 @@
 // Save Color In LocalStorage
-let mainColor = localStorage.getItem("color-option");
+let mainColor = localStorage.getItem("color_option");
 if (mainColor !== null) {
   document.documentElement.style.setProperty("--main-color", mainColor);
+  document.querySelectorAll(".colors-list li").forEach(element => {
+    element.classList.remove("active");
+    if (element.dataset.color === mainColor) {
+      element.classList.add("active");
+    }
+  });
+}
+
+let backgroundOption = true;
+let backgroundInterval;
+
+let backgroundLocalItem = localStorage.getItem("background_option");
+
+// Remove Active Class From All Spans
+if (backgroundLocalItem !== null) {
+  document.querySelectorAll(".random-backgrounds span").forEach(element => {
+    element.classList.remove("active");
+  });
+  if (backgroundLocalItem === "true") {
+    backgroundOption = true;
+    document.querySelector(".random-backgrounds .yes").classList.add("active");
+  } else {
+    backgroundOption = false;
+    document.querySelector(".random-backgrounds .no").classList.add("active");
+  }
 }
 
 // Click On Toggle Settings Icon
@@ -15,7 +40,7 @@ const colorsLi = document.querySelectorAll(".colors-list li");
 colorsLi.forEach(li => {
   li.addEventListener("click", (e) => {
     document.documentElement.style.setProperty("--main-color", e.target.dataset.color);
-    localStorage.setItem("color-option", e.target.dataset.color);
+    localStorage.setItem("color_option", e.target.dataset.color);
     e.target.parentElement.querySelectorAll(".active").forEach(element => {
       element.classList.remove("active");
     });
@@ -23,11 +48,36 @@ colorsLi.forEach(li => {
   });
 });
 
+const randomBackground = document.querySelectorAll(".random-backgrounds span");
+randomBackground.forEach(span => {
+  span.addEventListener("click", (e) => {
+    e.target.parentElement.querySelectorAll(".active").forEach(element => {
+      element.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    if (e.target.dataset.background === "yes") {
+      backgroundOption = true;
+      randomizeImgs();
+      localStorage.setItem("background_option", true);
+    } else {
+      backgroundOption = false;
+      clearInterval(backgroundInterval);
+      localStorage.setItem("background_option", false);
+    }
+  });
+});
+
 // Random Background Images
 let landingPage = document.querySelector(".landing-page");
 let imagesArray = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
 
-setInterval(() => {
-  let randomImag = Math.floor(Math.random() * imagesArray.length);
-  landingPage.style.backgroundImage = 'url("imgs/' + imagesArray[randomImag] + '")';
-}, 10000);
+function randomizeImgs() {
+  if (backgroundOption === true) {
+    backgroundInterval = setInterval(() => {
+      let randomImag = Math.floor(Math.random() * imagesArray.length);
+      landingPage.style.backgroundImage = 'url("imgs/' + imagesArray[randomImag] + '")';
+    }, 10000);
+  }
+}
+
+randomizeImgs();
